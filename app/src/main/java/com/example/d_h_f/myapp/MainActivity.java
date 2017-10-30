4251e7e712d43477e8c7fdfe1008137f5279129d
d-h-f <106937269@qq.com>
2017-10-30 19:21:10 +0800

    new class

--- app/src/main/java/com/example/d_h_f/myapp/MainActivity.java
+++ app/src/main/java/com/example/d_h_f/myapp/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //8888888888888
+        //777777777777777777
     }
 }
