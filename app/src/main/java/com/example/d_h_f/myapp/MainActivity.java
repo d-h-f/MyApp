490947833f0b9526b28a04d8500f8bdafb3a23cb
d-h-f <33215710+d-h-f@users.noreply.github.com>
2017-10-30 19:22:09 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/d_h_f/myapp/MainActivity.java
+++ app/src/main/java/com/example/d_h_f/myapp/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //777777777777777777
+        //999999999999999999
     }
 }
