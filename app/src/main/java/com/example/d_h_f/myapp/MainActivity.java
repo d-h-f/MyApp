680c91d9a389cc443c23d2f675032b57944c6710
d-h-f <106937269@qq.com>
2017-10-30 19:13:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/d_h_f/myapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.d_h_f.myapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //11111111111
+    }
+}
